4f19d9f0504ca8795bb4a97cf42a3e23334474dd
error0 <599727173@qq.com>
2021-02-01 16:48:18 +0800

    添加测试模块

---
+++ test/test.cpp
@@ -0,0 +1,50 @@
+#include <fstream>
+#include <iostream>
+#include "Lexer.hpp"
+#include "Parser.hpp"
+using std::cout;
+using std::cin;
+using std::endl;
+using std::ifstream;
+
+int main(int argc,char  **args)
+{   
+
+   if(argc!=2)
+   {
+    cout<<"args is error"<<endl;
+    return -1;
+   }
+   
+   ifstream infile;
+   infile.open(args[1]);
+   if(!infile.is_open())
+   {
+     cout<<args[1]<<":file is error"<<endl;
+     return -1;
+   }
+
+   /* 读取文件*/
+   int length;
+   char *buffer=nullptr;
+   infile.seekg(0,std::ios::end);
+   length=infile.tellg();
+   infile.seekg(0,std::ios::beg);
+   buffer=new char[length];
+   buffer[length]='\0';
+   infile.read(buffer,length);
+   infile.close();
+
+
+/*-----------------buffer-test-start----------------------*/
+   cout<<buffer<<endl;  
+/*-----------------buffer-test-end-----------------------*/
+
+/*-----------------Parser-test-start----------------------*/
+   Lexer lexer=Lexer(buffer);
+   Parser parser(&lexer);
+   bool t=parser.parse();
+/*-----------------Parser-test-end-----------------------*/
+   delete[] buffer;
+   return 0;
+}
